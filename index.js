e7f998b3ca8220174710602a1ad60b7973a341e9
hastoprak <yhastoprak@gmail.com>
2024-12-20 13:05:14 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", {});
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
+});
